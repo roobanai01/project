9bcc9b7d2ebfbdb6176a5cfe2068b9423a867baf
Rooban Kumar K <roobanai01@brightskills.com>
2024-02-19 13:32:15 +0530

    todays task

--- python/gui.py
+++ python/gui.py
@@ -91,22 +91,49 @@ mainloop()'''
 
 
 from tkinter import *
+from tkinter import ttk
 win=Tk()
 win.title("www.personaldetails.com")
 win.geometry("500x500+500+500")
 win.state("zoomed")
 
 def Submit():
-    a=tbEntrya
-    b=tbEntryb
-    c=tbEntryc
-    d=tbEntryd
-    e=tbEntrye
-    f=a+b+c+d+e
-    labeloutput.config(text=f)
+    a=tbEntrya.get()
+    #b=tbEntryb.get()
+    b=combo.get()
+    c=tbEntryc.get()
+    d=tbEntryd.get()
+    e=tbEntrye.get()
+    f=tbEntrye.get()
+    labeloutput1.config(text=a)
+    labeloutput2.config(text=b)
+    labeloutput3.config(text=c)
+    labeloutput4.config(text=d)
+    labeloutput5.config(text=e)
+    labeloutput5.config(text=f)
+def Clear():
+    a=" "
+    b=" "
+    c=" "
+    d=" "
+    e=" "
+    f=" "
+    labeloutput1.config(text=a)
+    labeloutput2.config(text=b)
+    labeloutput3.config(text=c)
+    labeloutput4.config(text=d)
+    labeloutput5.config(text=e)
+    labeloutput5.config(text=f)
+def option_selected(event):
+   selected_option = combo.get()
 
+def optionselected(event):
+   selected_option = combo2.get()
 
-Labeltitle=Label(win,text="Personal Details Form",font=("ALGERIAN",20),fg="Blue")
+def optionselected(event):
+   selected_option = combo3.get()
+
+Labeltitle=Label(win,text="Personal Details Form",font=("ALGERIAN",20),fg="White",bg="Blue")
 Labeltitle.grid(row=0,column=25)
 
 label1msg=Label(win,text="Name",font=("Batang",14),fg="black")
@@ -114,10 +141,15 @@ label1msg.grid(row=1,column=20,pady=10)
 tbEntrya=Entry(win, width=60)
 tbEntrya.grid(row=1,column=25)
 
-label2msg=Label(win,text="Gender",font=("Batang",14),fg="black")
+'''label2msg=Label(win,text="Gender",font=("Batang",14),fg="black")
 label2msg.grid(row=2,column=20,pady=10)
 tbEntryb=Entry(win, width=60)
-tbEntryb.grid(row=2,column=25)
+tbEntryb.grid(row=2,column=25)'''
+
+label2msg=Label(win,text="Gender",font=("Batang",14),fg="black")
+label2msg.grid(row=2,column=20,pady=10)
+combo=ttk.Combobox(win,values=["Male","Female","Others"])
+combo.grid(row=2,column=25,pady=10)
 
 label3msg=Label(win,text="D.O.B",font=("Batang",14),fg="black")
 label3msg.grid(row=3,column=20,pady=10)
@@ -126,21 +158,37 @@ tbEntryc.grid(row=3,column=25)
 
 label4msg=Label(win,text="Birth City",font=("Batang",14),fg="black")
 label4msg.grid(row=4,column=20,pady=10)
-tbEntryd=Entry(win, width=60)
-tbEntryd.grid(row=4,column=25)
-
-label5msg=Label(win,text="Phone No.",font=("Batang",14),fg="black")
+'''tbEntryd=Entry(win, width=60)
+tbEntryd.grid(row=4,column=25)'''
+combo2=ttk.Combobox(win,values=["Chennai", "Coimbatore", "Cuddalore", "Dharmapuri", "Dindigul", "Erode", "Kanchipuram", "Kanyakumari", "Karur", "Krishnagiri", "Madurai", "Nagapattinam", "Namakkal", "Perambalur", "Pudukkottai", "Ramanathapuram", "Salem", "Sivaganga", "Thanjavur", "The Nilgiris", "Theni", "Thoothukudi", "Tiruchirapalli", "Tirunelveli", "Tiruvallur", "Tiruvannamalai", "Tiruvarur", "Vellore", "Viluppuram", "Virudhunagar"])
+combo2.grid(row=4,column=25)
+label5msg=Label(win,text="State",font=("Batang",14),fg="black")
 label5msg.grid(row=5,column=20,pady=10)
-tbEntrye=Entry(win, width=60)
-tbEntrye.grid(row=5,column=25)
+#tbEntryd=Entry(win, width=60)
+#tbEntryd.grid(row=5,column=25)
+combo3=ttk.Combobox(win,values=["Andaman & Nicobar Island","Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chandigarh","Chhattisgarh","Dadra & Nagar Haveli","Daman & Diu","Delhi","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu & Kashmir","Jharkhand","Karnataka","Kerala","Lakshadweep","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Puducherry","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"])
+combo3.grid(row=5,column=25)
 
-labeloutput=Label(win,text=" ")
-labeloutput.grid(row=8,column=30,pady=20)
+label6msg=Label(win,text="Phone No.",font=("Batang",14),fg="black")
+label6msg.grid(row=6,column=20,pady=10)
+tbEntrye=Entry(win, width=60)
+tbEntrye.grid(row=6,column=25)
+
+labeloutput1=Label(win,text=" ")
+labeloutput1.grid(row=8,column=30,pady=15)
+labeloutput2=Label(win,text=" ")
+labeloutput2.grid(row=9,column=30,pady=15)
+labeloutput3=Label(win,text=" ")
+labeloutput3.grid(row=10,column=30,pady=15)
+labeloutput4=Label(win,text=" ")
+labeloutput4.grid(row=11,column=30,pady=15)
+labeloutput5=Label(win,text=" ")
+labeloutput5.grid(row=12,column=30,pady=15)
 
 btnsub=Button(win,text="Submit",font=("Batang",14),fg="White",bg="Green",command=Submit)
 btnsub.grid(row=7,column=1)
 
-btnclr=Button(win,text="Clear",font=("Batang",14),fg="White",bg="Blue")
+btnclr=Button(win,text="Clear",font=("Batang",14),fg="White",bg="Blue", command=Clear)
 btnclr.grid(row=7,column=2)
 
 btnclo=Button(win,text="Close",font=("Batang",14),fg="White",bg="Red")
